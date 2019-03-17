7e80148a2e799d00d844df54d16acdd3bb13da52
DESKTOP-O5CB4JR\dsx <1553267648@qq.com>
2019-03-17 17:59:27 +0800

    获取豆瓣的接口数据，并绑定到控件离

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,66 +1,69 @@
 // pages/movies/movies.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var inTheaterUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3"
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3"
+    var top250 = app.globalData.doubanBase + 
+      "/v2/movie/top250" + "?start=0&count=3"
+    
+    this.getMovieListData(inTheaterUrl, "inTheaters")
+    this.getMovieListData(comingSoonUrl, "comingSoon")
+    this.getMovieListData(top250, "top250")
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getMovieListData: function(url, settedKey){
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        console.log(res)
+        that.processDouban(res, settedKey)
+      },
+      fail: function () {
+        console.log('fail')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDouban: function (moviesDouban, settedKey){
+    var movies = []
+    for(var idx in moviesDouban.data.subjects){
+      var subject = moviesDouban.data.subjects[idx]
+      var title = subject.title
+      if(title.length >= 6){
+        title = title.substring(0, 6) + "..."
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    var readyData = {}
+    readyData[settedKey] = {
+      movies: movies
+    }
+    this.setData(readyData)
   }
+
 })
